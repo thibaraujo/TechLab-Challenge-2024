6f83e7927043cf34149e05640189efbdd0f7f6aa
thibaraujo <thiagobatistaaraujo06@gmail.com>
2024-07-03 17:36:39 -0300

    feat: add dark and light mode

--- frontend-backoffice/src/App.tsx
+++ frontend-backoffice/src/App.tsx
@@ -4,20 +4,26 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { ThemeProvider } from '@emotion/react';
-import { defaultTheme } from './themes/default.js';
+import { darkTheme, lightTheme, MaterialUISwitch} from './themes/default.js';
 import { Router } from './Router.js';
 import { CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthenticationProvider } from './contexts/AuthenticationContext.js';
+import { useState } from "react";
 
 const queryClient = new QueryClient()
 
 export function App() {
+  const [light, setLight] = useState(true);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={defaultTheme}>
+      
+      <ThemeProvider theme={light ? lightTheme : darkTheme}>
+        
         <CssBaseline />
         <AuthenticationProvider>
+          <MaterialUISwitch checked={light} onChange={() => setLight(prev => !prev)}/>
           <Router />
         </AuthenticationProvider>
       </ThemeProvider>
