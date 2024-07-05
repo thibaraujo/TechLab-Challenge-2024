47ed305b2c136e179a219df1a86918f1d0864345
thibaraujo <thiagobatistaaraujo06@gmail.com>
2024-07-04 23:26:53 -0300

    feat: add register screen

--- frontend-chat/src/PublicRouter.tsx
+++ frontend-chat/src/PublicRouter.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignIn from "./screens/login";
+import { RegisterScreen }from "./screens/RegisterScreen";
 
 const router = createBrowserRouter([
-  { path: '/*', element: <SignIn /> },
+  { path: '/', element: <SignIn /> },
+  { path: '/cadastro', element: <RegisterScreen /> },
 ])
 
 export function PublicRouter() {
