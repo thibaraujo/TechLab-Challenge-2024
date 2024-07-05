86f08de9c5556371aba8661a35775a7841e5485e
thibaraujo <thiagobatistaaraujo06@gmail.com>
2024-07-05 18:35:30 -0300

    feat: create edit and delete user into user list

--- frontend-backoffice/src/components/UserItem.tsx
+++ frontend-backoffice/src/components/UserItem.tsx
@@ -1,26 +1,125 @@
-import { ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Typography } from "@mui/material";
+import { 
+  ListItem, 
+  ListItemButton, 
+  ListItemIcon, 
+  ListItemText, 
+  Paper, 
+  Typography, 
+  Button, 
+  Dialog, 
+  DialogTitle, 
+  DialogContent, 
+  DialogContentText, 
+  DialogActions,
+  Grid,
+  Alert,
+  AlertTitle
+} from "@mui/material";
 import { IUser } from "../interfaces/IUser.js";
-import { Link } from "react-router-dom";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Delete from '@mui/icons-material/Delete';
+import ModeEdit from '@mui/icons-material/ModeEdit';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAccessToken } from "../hooks/useAuthenticationContext.js";
+import { api } from "../services/api";
 
 export interface IUserItemProps {
   user: IUser
 }
 
 export function UserItem({ user }: IUserItemProps) {
+  let navigate = useNavigate(); 
+  const [open, setOpen] = useState(false);
+  const [statusAlert, setStatusAlert] = useState(-1);
+  const [alert, setAlert] = useState(true);  
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const accessToken = useAccessToken();
+
+  const deleteUser = async () => {
+    const response = await api.delete(`/users`, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+      params: {
+        id: user._id
+      }
+    });
+
+    setStatusAlert(response.status);
+    setAlert(true);
+    setTimeout(() => {
+      setAlert(false);
+      //location.reload(); todo:testar com o token depois
+    }, 3000);
+
+    if(response.status == 200) {
+      navigate("/users", {replace: true});
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <Paper>
+    <Paper style={{width:"85vw"}}>
       <Typography variant="body1" gutterBottom>
-        <Link to={`/users/${user._id}`}>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <AccountCircleIcon />
-              </ListItemIcon>
-              <ListItemText primary={user.username} secondary={user.email}/>
-            </ListItemButton>
-          </ListItem>
-        </Link>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            <ListItemText primary={user.username} secondary={user.email}/>
+            <ListItemIcon></ListItemIcon>
+            <ListItemIcon onClick={() => navigate(`/users/${user._id}`)}>
+              <ModeEdit />
+            </ListItemIcon>
+            <ListItemIcon onClick={handleClickOpen}>
+              <Delete />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Excluir usuário"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Tem certeza que deseja excluir o usuário? 
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancelar</Button>
+            <Button onClick={deleteUser} autoFocus>
+              Excluir
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Grid item sm={10}>
+          {statusAlert == 200 ? 
+            alert && 
+            <Alert severity="success">
+              <AlertTitle>Sucesso</AlertTitle>
+              Usuário excluído com sucesso.
+            </Alert>
+            : statusAlert != -1 ? 
+            alert && 
+            <Alert severity="error">
+              <AlertTitle>Erro</AlertTitle>
+              Erro ao excluir usuário.
+            </Alert> : <></>
+          }
+        </Grid>
       </Typography>
     </Paper>
   )
